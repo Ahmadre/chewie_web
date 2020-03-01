63488b0d8e1efc96a892f718e579d8be4b463319
Ahmadre <rebar.ahmad@telexiom.de>
2020-03-01 03:28:22 +0100

    Update to master channe

--- lib/src/custom_chewie_player.dart
+++ lib/src/custom_chewie_player.dart
@@ -220,7 +220,7 @@ class _ChewiePlayerState extends State<Chewie> {
     leaveFullscreen = false;
     final isAndroid = Theme.of(context).platform == TargetPlatform.android;
     final TransitionRoute<Null> route = new PageRouteBuilder<Null>(
-      settings: new RouteSettings(isInitialRoute: false),
+      settings: new RouteSettings(),
       pageBuilder: _fullScreenRoutePageBuilder,
     );
 
